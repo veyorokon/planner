9fe7d95b0029be03ed90d2a8e8b3810e8312f1f0
veyor <veyor@DESKTOP-93D0BKC>
2016-12-09 23:28:28 -0500

    Correct creation of 2d map. Use map to output state.

--- Eyorokon_planner/Tree.h
+++ Eyorokon_planner/Tree.h
@@ -3,6 +3,8 @@
 #include "Relation.h"
 #include "Node.h"
 #include <fstream>
+#include <algorithm>
+
 #define block int
 #define holding 4
 using namespace std;
@@ -39,38 +41,112 @@ public:
 
 	/*Output the plan to a file*/
 	void saveOutput(Node & planNode) {
+
+		
 		output.open("output.txt");
 		Node * ptr = &planNode;
-		int count = 0;
+		int phase = 0;
 		while (ptr != NULL){
 			output << "\n";
-			output << "-------| " + to_string(count) + " |-------\n";
+			output << "-------| " + to_string(phase) + " |-------\n";
 			output << ptr->plan;
-			count++;
+			phase++;
 			ptr = ptr->parent;
 		}
 		output.close();
-
-		vector<int> order;
-		getOrdering(order, planNode.current);
-		cout << "Print order " << endl;
-		for (int i = 0; i < order.size(); i++) {
-			cout << order[i] << " ";
-		}
-
+		drawState(planNode.current, output);
+		
 	}
+
+
+	//http://stackoverflow.com/questions/890164/how-can-i-split-a-string-by-a-delimiter-into-an-array
+	vector<string> explode(const string& str, const char& ch) {
+		string next;
+		vector<string> result;
+
+		// For each character in the string
+		for (string::const_iterator it = str.begin(); it != str.end(); it++) {
+			// If we've hit the terminal character
+			if (*it == ch) {
+				// If we have some characters accumulated
+				if (!next.empty()) {
+					// Add them to the result vector
+					result.push_back(next);
+					next.clear();
+				}
+			}
+			else {
+				// Accumulate the next character into the sequence
+				next += *it;
+			}
+		}
+		if (!next.empty())
+			result.push_back(next);
+		return result;
+	}
+
 	void drawState(State & state, ofstream& myfile) {
 		string space = "       ";
-		
+		//create 2d map
+		int height = 7, width = 4;
+		string** array2D = 0;
+		array2D = new string*[height];
 
-		//if(held != -1)
-		//myfile << "                                               holding: Block"+to_string(held)+"\n";
-		
+		for (int h = 0; h < height; h++)
+		{
+			array2D[h] = new string[width];
+			for (int w = 0; w < width; w++)
+			{
+				array2D[h][w] = " ";
+			}
+		}
+
+		//PROCESS VECTOR
+		vector<int> order;
+		vector<int> heights;
+		getOrdering(heights,  order,state);
+		cout << "Print order " << endl;
+		string ordering = "";
+		for (int i = 0; i < order.size(); i++) {
+			cout << order[i] << " ";
+		}		
+		cout << endl;
+		for (int i = 0; i < heights.size(); i++) {
+			cout << heights[i] << " ";
+		}
+
+		int ptr =1;
+		int col = 4, row =6;
+		while( col >= 0 && ptr < order.size()) {
+			while( row >= 0 && ptr < order.size()) {
+				if (order[(order.size() - ptr)] == -1) {
+					col--;
+					row = 6;
+				}
+				else {
+					array2D[row][col] = to_string(order[(order.size()-ptr)]);
+					cout << "HKHK: " << array2D[row][col] << endl;
+					row -= 1;
+				}
+				ptr++;
+			}
+		}
+
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				cout << array2D[i][j] << " ";
+			}
+			cout << endl;
+		}
+
+
+		cout << endl;
+
 		myfile << "|-------------|--------------|--------------|--------------|\n";
 		myfile << "      l1              l2             l3             l4       \n";
 	}
 
-	void getOrdering(vector<int>& order, State & state) {
+	void getOrdering( vector<int>& heights, vector<int>& order, State & state) {
 		int ** locations = state.getLocations();
 		int held = locations[4][0];
 		vector<int> ordering;
@@ -78,7 +154,7 @@ public:
 		
 		for (int i = 0; i < 4; i++) {
 			block top = getTopBlock(locations, state, i); //Get the topmost block for row
-			addToVec(order, state, top);
+			addToVec(heights, order, state, top);
 		}
 	}
 
@@ -96,14 +172,20 @@ public:
 	}
 
 
-	void addToVec(vector<int>& order, State & state, block blk) {
+	void addToVec(vector<int>& heights, vector<int>& order, State & state, block blk) {
+		int height = 0;
 		order.push_back(blk);
-		if (blk == -1) return;
+		if (blk == -1) {
+			heights.push_back(height);
+			return;
+		}
 
 		while (blk != -1) {
+			height++;
 			blk = state.GetOn(blk);
 			order.push_back(blk); //check the next block
 		}
+		heights.push_back(height);
 		//order.push_back(-1); //end of row
 	}
 	
